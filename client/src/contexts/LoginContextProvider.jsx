90869fafb6b7037c0fe0743640e144851594c2ed
DoheeKim <ehgml7184@naver.com>
2024-06-27 20:25:00 +0900

    front-end: 유저 로그인을 위해 비동기 axios 세팅

---
+++ client/src/contexts/LoginContextProvider.jsx
@@ -0,0 +1,163 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import axios from '../apis/axios';
+import * as auth from '../apis/common/auth';
+import api from '../apis/axios';
+
+// 컨텍스트 생성
+export const LoginContext = createContext();
+
+const LoginContextProvider = ( {children} ) => {
+
+    /* -----------------------⬇ [State] ⬇---------------------------- */
+    // 👩‍💼🔐 로그인 여부
+    const [isLogin, setLogin] = useState(false);
+    // 유저 정보
+    const [userInfo, setUserInfo] = useState(null)
+    // 권한 정보
+    const [roles, setRoles] = useState({isUser : false, isAmdin : false})
+    
+    // 페이지 이동
+    const navigate = useNavigate();
+    
+    
+    /* -----------------------⬇ [functions] ⬇---------------------------- */
+    // 🔐✅로그인 체크
+    const loginCheck = async () => {
+
+        // accessToken 쿠키 확인
+        const accessToken = Cookies.get("accessToken");
+        console.log(`accessToken: ${accessToken}`);
+
+        // 쿠키 속에 토큰 존재❌
+        if (!accessToken) {
+            console.log('쿠키에 JWT(accessToken)이 없음.');
+
+            // 로그아웃 세팅
+            logoutSetting();
+
+            return;
+        }
+
+        // 쿠키 속에 토큰 존재⭕
+        console.log('쿠키에 JWT(accessToken)가 저장되어있음.');
+
+        // axios common header에 등록
+        // 한번 로그인이 되면 다음 요청부터는 autorization에 등록되기때문에 
+        axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+        // 👩‍💼 사용자 정보 요청
+        let response;
+        let data;
+
+        try {
+            // 사용자 정보를 요청하여 받아옴
+            // 이미 common header에 jwt토큰 담겨있어서 빈 채로 보내도 됨.
+            response = await auth.info();
+        } catch (error) {
+            console.log(`error: ${error}`);
+            console.log(`status: ${response.status}`);
+            return;
+        }
+
+        // data = 👩‍💼사용자 정보
+        data = response.data;   
+        console.log(`data: ${data}`);
+
+        // 에러코드 401: 인증받지않은 사용자
+        if ( data == 'UNAUTHORIZED' || response.status == 401) {
+            console.log(`accessToken(jwt)이 만료되었거나 인증에 실패하였습니다.`);
+            return;
+        }
+
+        // 인증 성공 ✅
+        console.log(`accessToken(jwt) 토큰으로 사용자 정보 요청 성공!`);
+
+        // 로그인 세팅
+        loginSetting(data, accessToken);
+    }
+
+
+    // 로그아웃
+    const logout = () => {
+        const check = window.confirm("정말로 로그아웃하시겠습니까?");
+
+        if (!check) {
+            return;
+        }
+        logoutSetting();
+        navigate("/")
+    }
+
+
+    // 로그아웃 세팅
+    const logoutSetting = () => {
+        // 🚀❌ axios 헤더 초기화
+        api.defaults.headers.common.Authorization = undefined;
+        // 🍪❌ 쿠키 초기화
+        Cookies.remove("accessToken")
+        // 🔐❌ 로그인 여부 : false
+        setLogin(false)
+        // 👩‍💼❌ 유저 정보 초기화
+        setUserInfo(null)
+        // 👮‍♀️❌ 권한 정보 초기화
+        setRoles(null)
+    }
+
+    // 🔐로그인 세팅
+    // 👩‍💼userData, 📀accessToken(jwt)
+    const loginSetting = (userData, accessToken) => {
+
+        // 👩‍💼Users (DTO) ➡ JSON 형식
+        const { no, userId, authList } = userData;
+        const roleList = authList.map( (auth) => auth.auth ); // authList라는 auth타입의 배열을 하나하나 꺼냄 = auth객체의 변수 auth를 꺼내옴
+
+        // 조회된 유저 정보 확인
+        console.log(`no : ${no}`);
+        console.log(`userId : ${userId}`);
+        console.log(`authList : ${authList}`);
+        console.log(`roleList : ${roleList}`);
+
+        // axios common header - Authorization 헤더에 jwt 등록
+        api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+        // Context에 정보 등록
+        // 🔐로그인 여부 세팅
+        setLogin(true);
+
+        // 👩‍💼유저 정보 세팅
+        const updatedUserInfo = {no, userId, roleList};
+        setUserInfo(updatedUserInfo);
+
+        // 👮‍♀️권한 정보 세팅
+        const updatedRoles = {isUser: false, isAmdin: false};
+
+        roleList.forEach( (role) => {
+            if (role == 'ROLE_USER') {
+                updatedRoles.isUser = true;        
+            }
+            if (role == 'ROLE_ADMIN') {
+                updatedRoles.isAmdin = true;        
+            }
+        });
+
+    }
+
+
+    
+    
+    /* ------------------------------------------------------------------ */
+
+
+  return (
+
+    // 컨텍스트 지정 -> value={?, ?}
+    <LoginContext.Provider value={ {isLogin, logout} }>
+        {children}
+    </LoginContext.Provider>
+
+  )
+}
+
+export default LoginContextProvider
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/common/Header'
-import Footer from '../components/common/Footer'
+import Header from '../layout/Header'
+import Footer from '../layout/Footer'
 
 const Home = () => {
   return (
