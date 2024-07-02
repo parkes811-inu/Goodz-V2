2c0cb4831936b9fbcaf028659808e9bc62c1567e
parkes811 <parkes811@naver.com>
2024-07-02 16:03:35 +0900

    React : 메인 화면 페이지 구현

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,20 @@ import Carousel from '../components/product/Carousel'
 import NewArrivals from '../components/product/NewArrivals';
 import PopularPosts from '../components/product/PopularPosts';
 
+const imageList = [
+  { no: 1, name: 'main1.png' },
+  { no: 2, name: 'main2.png' },
+  { no: 3, name: 'main3.png' },
+  { no: 4, name: 'main4.png' },
+  { no: 5, name: 'main5.png' },
+  { no: 6, name: 'main6.png' }
+];
+
 const Home = () => (
   <MainLayout>
     <div className="container">
-      <Carousel />
       <div className="container mt-4">
+        <Carousel imageList={imageList}/>
         <NewArrivals />
         <PopularPosts />
       </div>
