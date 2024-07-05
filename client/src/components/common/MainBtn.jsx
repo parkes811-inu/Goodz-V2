908859b4f19b474f2bb6b59b1dd7146411c43b31
DoheeKim <106290302+Dozzykim@users.noreply.github.com>
2024-07-05 10:19:50 +0900

    Update MainBtn.jsx

--- client/src/components/common/MainBtn.jsx
+++ client/src/components/common/MainBtn.jsx
@@ -5,7 +5,7 @@ const MainBtn = ( {text} ) => {
   return (
 
     <div className="py-2">
-        <button type="button" className="mainbtn btn btn-dark w-100" >{text}</button>
+        <button type="submit" className="mainbtn btn btn-dark w-100" >{text}</button>
     </div>
 
   )
